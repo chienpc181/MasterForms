d9a9935962f25fda629537c58422b6d4325fb756
chien pham <chien@rangstrup.com>
2023-09-03 18:56:29 +0700

    Add property - BankUser - Address

--- Entities/BankUser.cs
+++ Entities/BankUser.cs
@@ -11,5 +11,6 @@ namespace MasterForms.Entities
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Address { get; set; }
     }
 }
